afe8a9544abaf9e9b14efe34f68867503125a8a7
MrNovan <125030201+MrNovan@users.noreply.github.com>
2023-06-06 17:25:05 +0300

    Delete greeting.js

--- bin/greeting.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const greeting = () => {
-  console.log('Добро пожаловать в игру крестики нолики.');
-  const userNameFirst = readlineSync.question('Могу я узнать как зовут первого игрока?');
-  const userNameSecond = readlineSync.question('Как зовут второго игрока?');
-  console.log(`Привет ${userNameFirst} и ${userNameSecond}, давайте сыграем в игру.`);
-};
-export default greeting;
