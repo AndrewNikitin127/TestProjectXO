e413a67736ed8f4bee4e51508acc48f99951887c
AndrewNikitin127 <donandrain153@gmail.com>
2023-06-08 15:08:42 +0500

    refactor ticTacToe

--- src/games/ticTacToe.js
+++ src/games/ticTacToe.js
@@ -1,4 +1,78 @@
-import readlineSync from 'readline-sync';
+import { getRandomInt, askQuestionRange } from '../tools.js';
+
+const printBoard = (board) => {
+  console.log(`\n ${board[0][0]} | ${board[0][1]} | ${board[0][2]} `);
+  console.log('-----|-----|-----');
+  console.log(` ${board[1][0]} | ${board[1][1]} | ${board[1][2]} `);
+  console.log('-----|-----|-----');
+  console.log(` ${board[2][0]} | ${board[2][1]} | ${board[2][2]} \n`);
+};
+
+const boardHasEmptyCell = (board, emptyCell) => {
+  let freeSpaces = 9;
+  board.forEach((lineArr) => lineArr.forEach((cell) => {
+    if (cell !== emptyCell) freeSpaces -= 1;
+  }));
+  return freeSpaces > 0;
+};
+
+const gameCanContinue = (winner, board, emptyCell) => (
+  winner === emptyCell && boardHasEmptyCell(board, emptyCell));
+
+const checkWinner = (board, emptyCell) => {
+  // check for rows
+  for (let i = 0; i < 3; i += 1) {
+    if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+      return board[i][0];
+    }
+  } // check for column
+  for (let i = 0; i < 3; i += 1) {
+    if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+      return board[0][i];
+    }
+  } // check for diagonals
+  if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    return board[0][0];
+  }
+  if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+    return board[0][2];
+  }
+  return emptyCell;
+};
+
+const printWinner = (winner, charPlayer1, charComputer) => {
+  if (winner === charPlayer1) {
+    console.log(' вы победили');
+  } else if (winner === charComputer) {
+    console.log('вы проиграли');
+  } else {
+    console.log('ничья');
+  }
+};
+
+const getPlayerMove = (board, emptyCell) => {
+  let x;
+  let y;
+  do {
+    console.log('ваш ход\n');
+    x = askQuestionRange('ведите номер строки #(1-3): ', 1, 3) - 1;
+    y = askQuestionRange('ведите номер строки #(1-3): ', 1, 3) - 1;
+
+    if (board[x][y] !== emptyCell) console.log('Ячейка уже занята');
+  } while (board[x][y] !== emptyCell);
+  return [x, y];
+};
+
+const getComputerMove = (board, emptyCell) => {
+  let x;
+  let y;
+
+  do {
+    x = getRandomInt(0, 2);
+    y = getRandomInt(0, 2);
+  } while (board[x][y] !== emptyCell);
+  return [x, y];
+};
 
 export default () => {
   const emptyCell = '   ';
@@ -7,122 +81,22 @@ export default () => {
     [emptyCell, emptyCell, emptyCell],
     [emptyCell, emptyCell, emptyCell],
   ];
-
   const charPlayer1 = ' X ';
   const charComputer = ' 0 ';
-
   let winner = emptyCell;
 
-  const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  while (gameCanContinue(winner, board, emptyCell)) {
+    printBoard(board);
+    const [x, y] = getPlayerMove(board, emptyCell);
+    board[x][y] = charPlayer1;
+    winner = checkWinner(board, emptyCell);
+    if (!gameCanContinue(winner, board, emptyCell)) break;
 
-  const question = (questWord) => {
-    const result = readlineSync.question(`Введите номер ${questWord} #(1-3): `, {
-      limit: /^[1-3]$/g,
-      limitMessage: 'Извините, $<lastInput> не подходит.',
-    });
-    return result - 1;
-  };
-
-  const printBoard = () => {
-    console.log();
-    console.log(` ${board[0][0]} | ${board[0][1]} | ${board[0][2]} `);
-    console.log('-----|-----|-----');
-    console.log(` ${board[1][0]} | ${board[1][1]} | ${board[1][2]} `);
-    console.log('-----|-----|-----');
-    console.log(` ${board[2][0]} | ${board[2][1]} | ${board[2][2]} `);
-    console.log();
-  };
-
-  const checkFreeSpace = () => {
-    let freeSpaces = 9;
-
-    board.forEach((lineArr) => lineArr.forEach((cell) => {
-      if (cell !== emptyCell) freeSpaces -= 1;
-    }));
-    return freeSpaces > 0;
-  };
-
-  const playerMove = () => {
-    let x;
-    let y;
-
-    do {
-      console.log('ваш ход\n');
-
-      x = question('строки');
-      y = question('ячейки');
-
-      if (board[x][y] !== emptyCell) {
-        console.log('Ячейка уже занята');
-      } else {
-        board[x][y] = charPlayer1;
-        break;
-      }
-    } while (board[x][y] !== emptyCell);
-  };
-
-  const printWinner = () => {
-    if (winner === charPlayer1) {
-      console.log(' вы победили');
-    } else if (winner === charComputer) {
-      console.log('вы проиграли');
-    } else {
-      console.log('ничья');
-    }
-  };
-  const computerMove = () => {
-    let x;
-    let y;
-
-    if (checkFreeSpace()) {
-      do {
-        x = getRandomInt(0, 2);
-        y = getRandomInt(0, 2);
-      } while (board[x][y] !== emptyCell);
-
-      board[x][y] = charComputer;
-    } else {
-      printWinner(emptyCell);
-    }
-  };
-  const checkWinner = () => {
-    // check for rows
-    for (let i = 0; i < 3; i += 1) {
-      if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
-        return board[i][0];
-      }
-    }
-    // check for column
-    for (let i = 0; i < 3; i += 1) {
-      if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
-        return board[0][i];
-      }
-    }
-    // check for diagonals
-    if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-      return board[0][0];
-    }
-    if (board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
-      return board[0][2];
-    }
-    return emptyCell;
-  };
-
-  while (winner === emptyCell && checkFreeSpace()) {
-    printBoard();
-    playerMove();
-    winner = checkWinner();
-
-    if (winner !== emptyCell || !checkFreeSpace()) {
-      break;
-    }
-    computerMove();
-    winner = checkWinner();
-
-    if (winner !== emptyCell || !checkFreeSpace()) {
-      break;
-    }
+    const [a, z] = getComputerMove(board, emptyCell);
+    board[a][z] = charComputer;
+    winner = checkWinner(board, emptyCell);
+    if (!gameCanContinue(winner, board, emptyCell)) break;
   }
-  printBoard();
-  printWinner(winner);
+  printBoard(board);
+  printWinner(winner, charPlayer1, charComputer);
 };
--- src/tools.js
+++ src/tools.js
@@ -1,2 +1,18 @@
+import readlineSync from 'readline-sync';
 // тут храним и экспортируем
 // самописные функции, и константы, которые используем больше чем в одном файле
+
+/**
+ * @param {num} min @param {num} max @returns {num} random number */
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+/**
+ * displays the text and asks you to enter a number in the range
+ * @param {string} question @param {int} begin @param {int} end
+ * @returns {int} number from the user */
+const askQuestionRange = (question, begin, end) => +readlineSync.question(question, {
+  limit: new RegExp(`^[${begin}-${end}]$`),
+  limitMessage: 'Извините, $<lastInput> не подходит.',
+});
+
+export { getRandomInt, askQuestionRange };
